c63b9174318405fd9e65c978d1c7efe7f1771d27
SkieeL <Skiel_Mafu-cs@hotmail.com>
2019-09-05 16:14:21 -0300

    Se resuelve el ejercicio de consolidación de la clase 2

--- clase_2/trivia_inutil.py
+++ clase_2/trivia_inutil.py
@@ -1,13 +1,13 @@
-nombre = input("Ingrese su nombre: ")
-edad = input("Ingrese su edad: ")
-peso = input("Ingrese su peso: ")
+nombre = input("Hola. ¿Cuál es tu nombre? ")
+edad = int(input("¿Cuántos años tenés? "))
+peso = int(input("Ok. Última pregunta: ¿Cuánto pesas? "))
 
-print("Si no te funcionara el Shif tu nombre se vería", nombre.lower())
-print("Si se te trabara el CapsLock tu nombre se vería", nombre.upper())
-print("Si un nene chiquito te quisisera llamar la atención te diría", nombre * 5)
-print("Tu edad es de", edad * 365 * 24 * 60 * 60, "segundos")
-print("Sabías que en la Luna pesarías sólo", peso / 6, "kilos")
-print("Ahora bien, en el Sol, tu peso sería de", peso * 27.1, "kilos (aunque no por mucho tiempo)")
+print("\nSi no te funcionara el Shift, tu nombre se escribiría", nombre.lower())
+print("\nAhora, si tuvieses el Caps Lock trabado, sería", nombre.upper())
+print("\nSi un nene chiquito te quisiera llamar la atención, tu nombre se volvería:\n" + nombre * 5)
+print("\nTu edad es de", edad * 365 * 24 * 60 * 60, "segundos.")
+print("\n¿Sabías que en la Luna pesarías sólo", peso / 6, "kilos?")
+print("Ahora bien, en el Sol, tu peso sería de", peso * 27.1, "kilos (aunque no por mucho tiempo)\n")
 
 input("Presione ENTER para continuar")
 
